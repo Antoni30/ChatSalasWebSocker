f2c9d04b98f0edd4d95ce1741148988e6f3197f3
Antoni30 <artoapanta3@espe.edu.ec>
2025-05-21 10:52:36 -0500

    deploy

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,6 +1,6 @@
 // socket.ts
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://10.40.14.75:8080");
+const socket: Socket = io("back-chat-production-ea5d.up.railway.app");
 
 export default socket;
